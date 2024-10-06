f1dba71dd99cfee17b624f420696c6f36bd7aa09
PedroSilva247 <pedrosilveiradasilva552@gmail.com>
2024-10-06 13:48:23 -0300

    bug-do-form-consertado

--- frontend/src/components/formNewClient/FormNewClient.jsx
+++ frontend/src/components/formNewClient/FormNewClient.jsx
@@ -28,6 +28,12 @@ function FormNewClient() {
             elements = form.getElementsByClassName("isInvalid")
             elements[0].classList.remove("isInvalid")
         }
+
+        setNewClientName("")
+        setNewClientEmail("")
+        setNewClientAddress("")
+        setNewClientPhone("")
+        setNewClientCPForCNPJ("")
     }
 
     const handleSubmit = (event) => {
@@ -93,11 +99,11 @@ function FormNewClient() {
                     <div className='radiosCPForCNPJ'>
                         <label htmlFor="cpf" className='labelRadiosCpfCnpj'>
                             <input type="radio" value={0}  name="cpfCnpj" id="cpf" className='inputRadioCpfCnpj'
-                            onChange={() => setOption("cpf")} checked />
+                            onClick={() => setOption("cpf")} defaultChecked/>
                             <label className='text labelRadio' htmlFor='cpf'>CPF</label>
                         </label>
                         <label htmlFor="cnpj" className='labelRadiosCpfCnpj'>
-                            <input type="radio" value={0} name="cpfCnpj" id="cnpj" className='inputRadioCpfCnpj' onChange={() => setOption("cnpj")}/>
+                            <input type="radio" value={0} name="cpfCnpj" id="cnpj" className='inputRadioCpfCnpj' onClick={() => setOption("cnpj")}/>
                             <label className='text labelRadio' htmlFor='cnpj'>CNPJ</label>
                         </label>
 
