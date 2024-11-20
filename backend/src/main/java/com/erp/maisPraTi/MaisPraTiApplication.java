85a0df4e7a14f9feaa6e3a430ed2d59400c435ca
fernando-angeli <fernando_angeli@hotmail.com>
2024-11-20 00:03:34 -0300

    update application.properties

--- backend/src/main/java/com/erp/maisPraTi/MaisPraTiApplication.java
+++ backend/src/main/java/com/erp/maisPraTi/MaisPraTiApplication.java
@@ -1,5 +1,6 @@
 package com.erp.maisPraTi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MaisPraTiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("jwt.secret", dotenv.get("JWT_SECRET"));
+		System.setProperty("jwt.expiration", dotenv.get("JWT_EXPIRATION"));
+		System.setProperty("jwt.expiration-recovery", dotenv.get("JWT_EXPIRATION_RECOVERY"));
+		System.setProperty("spring.profiles.active", dotenv.get("APP_PROFILE"));
+		System.setProperty("spring.mail.username", dotenv.get("EMAIL_PASSWORD"));
+		System.setProperty("spring.mail.password", dotenv.get("EMAIL_PASSWORD"));
+
 		SpringApplication.run(MaisPraTiApplication.class, args);
 	}
 
